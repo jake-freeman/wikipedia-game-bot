d8ddc83c0c437446e544461e90608ad76e1b94f5
jake-freeman <Jacob.Freeman@vorne.com>
2020-05-09 18:03:11 -0500

    BFS algo for finding page path

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ function makeApiLink(pageName) {
     return `https://en.wikipedia.org/api/rest_v1/page/mobile-sections/${pageName}`;
 }
 
+let requests = 0;
+
 async function fetchArtcleText(url) {
-    const res = await fetch(url);
-    const json = await res.json();
+    let res;
+    let json;
+
+    requests++;
+
+    try {
+        res = await fetch(url);
+        json = await res.json();
+    }
+    catch(e) {
+        return '';
+    }
     
-    let sections = json.lead.sections;
+    let sections = (json.lead && json.lead.sections) || [];
 
     if (json.remaining && json.remaining.sections) {
         sections = sections.concat(json.remaining.sections);
@@ -42,7 +54,7 @@ async function fetchArticleLinkNames(url) {
     });
 
     urls = urls.filter((url) => {
-        return url.startsWith('/wiki');
+        return url.startsWith('/wiki/') && !url.startsWith('/wiki/File');
     });
 
     const articleNames = urls.map((url) => {
@@ -52,11 +64,59 @@ async function fetchArticleLinkNames(url) {
     return articleNames;
 }
 
-async function main() {
-    const targetPageName = 'World_War_II';
-    const linkedArticleNames = await fetchArticleLinkNames(makeApiLink('Zvi_Bern'));
+function printPath(path) {
+    console.log('Found path:');
+    console.log(path.join(' -> '));
+    console.log(`In only ${requests} requests`);
+}
 
-    console.log(linkedArticleNames);
+async function searchForTarget(startName, targetName) {
+    let queue = [
+        [startName]
+    ];
+
+    let found = (startName === targetName);
+
+    const explored = new Set();
+    explored.add(startName);
+
+    while (queue.length) {
+        const path = queue.shift();
+        const node = path[path.length - 1];
+
+        const edges = await fetchArticleLinkNames(makeApiLink(node));
+
+        const uniqueEdges = edges.filter((edge) => !explored.has(edge));
+
+        for (const edge of uniqueEdges) {
+            if (edge === targetName) {
+                found = true;
+
+                return path.concat([targetName]);
+            }
+
+            queue.push([
+                ...path,
+                edge
+            ]);
+
+            explored.add(edge);
+        }
+    }
+}
+
+async function main() {
+    const startingPageName = 'Zvi_Bern';
+    const targetPageName = 'World_War_II';
+
+    const path = await searchForTarget(startingPageName, targetPageName);
+
+    if (path) {
+        printPath(path);
+    }
+    else {
+        console.error('Failed to find a path');
+    }
 }
 
 main();
